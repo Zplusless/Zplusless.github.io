9117d9a95038f657a1728b593cf7a75157b33a99
Zplusless <liyuanzhe351@163.com>
2019-08-22 11:32:41 +0800

    debug:脚本中生成TOC未指向网页

--- deploy_script/generate.py
+++ deploy_script/generate.py
@@ -8,7 +8,7 @@ print('generating TOC')
 
 for file in os.listdir('.'):
     if 'html' in file:
-        lines.append('[{}]({})'.format(file,file)+'\n')
+        lines.append('[{}]({})'.format(file,'https://Zplusless.github.io/'+file)+'\n')
 
 with open('./README.MD', 'w') as readme:
     readme.writelines(lines)
