73157f55b99c4049802e4ed87f56496a356fb15c
zplusless <liyuanzhe351@163.com>
2020-11-02 17:47:29 +0800

    update

--- deploy_script/generate.py
+++ deploy_script/generate.py
@@ -8,7 +8,7 @@ print('generating TOC')
 
 for file in os.listdir('.'):
     if 'html' in file:
-        lines.append('[{}]({})'.format(file,'https://Zplusless.github.io/'+file)+'\n')
+        lines.append('[{}]({})'.format(file,'https://Zplusless.github.io/'+file)+'\n\n')
 
 with open('./README.MD', 'w') as readme:
     readme.writelines(lines)
